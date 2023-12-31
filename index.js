c9fba5fcc6a346a700f970641d886f4b1b249b9b
Xavier Lefèvre <xavierlefevre@xaviers-mbp.home>
2023-12-31 14:50:39 +0100

    project-initialization

---
+++ index.js
@@ -0,0 +1,7 @@
+class Block {
+    constructor(timestamp = '', data = []) {
+        this.timestamp = timestamp
+        // this.data should contain information like transactions.
+        this.data = data
+    }
+}
